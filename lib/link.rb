c3bf1a26e9cf1cddc53a0fb32ab0301f7b54d4e1
Nic Yeeles <n.yeeles@gmail.com>
2014-06-17 11:07:35 +0100

    Can add tags

--- lib/link.rb
+++ lib/link.rb
@@ -9,5 +9,5 @@ class Link
 	property :id,			Serial # Serial means that it will be auto-incremented for every record
 	property :title, 	String
 	property :url,		String
-
+	has n, :tags, :through => Resource
 end
--- lib/server.rb
+++ lib/server.rb
@@ -7,7 +7,7 @@ env = ENV["RACK_ENV"] || "development"
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require './lib/link' # this needs to be done after datamapper is initialised
-
+require './lib/tag'
 # After declaring your models, you should finalise them
 DataMapper.finalize
 # However, the database tables don't exist yet. Let's tell datamapper to create them
@@ -22,6 +22,17 @@ end
 post '/links' do
 	url = params["url"]
 	title = params["title"]
-	Link.create(:url => url, :title => title)
+	tags = params["tags"].split(" ").map do |tag|
+	#this will either find this tag or create
+	#it if it doesn't exist already
+	Tag.first_or_create(:text => tag)
+	end
+	Link.create(:url => url, :title => title, :tags => tags)
 	redirect to('/')
+end
+
+get '/tags/:text' do
+	tag = Tag.first(:text => params[:text])
+	@links = tag ? tag.links : []
+	erb :index
 end
