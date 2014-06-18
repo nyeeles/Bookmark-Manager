55e0d18373c50a6cfade7ff6f34d39610de300fd
Nic Yeeles <n.yeeles@gmail.com>
2014-06-18 11:40:44 +0100

    Includes password confirmation

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -13,18 +13,31 @@ feature "User signs up" do
   # to keep the example simple.
 
   scenario "when being logged out" do    
-    lambda { sign_up }.should change(User, :count).by(1)    
+    expect{ sign_up }.to change(User, :count).by 1
+    # previous line is the same as:
+    # user_count = User.all.count
+    # sign_up
+    # expect(User.count).to eq(user_count +1)    
     expect(page).to have_content("Welcome, alice@example.com")
     expect(User.first.email).to eq("alice@example.com")        
   end
 
-  def sign_up(email = "alice@example.com",
-  						password = "oranges!")
-  	visit '/users/new'
-  	expect(page.status_code).to eq(200)
-  	expect(page.status_code).to eq(200)
-  	fill_in :email, :with => email
-  	fill_in :password, :with => password
-  	click_button "Sign up"
+  scenario "with a password that doesn't match" do
+  	lambda { sign_up('a@a.com', 'pass', 'wrong')}.should change(User, :count).by(0)
   end
+
+  def sign_up(email = "alice@example.com", 
+              password = "oranges!", 
+              password_confirmation = "oranges!")
+    visit '/users/new'
+    fill_in :email, :with => email
+    fill_in :password, :with => password
+    fill_in :password_confirmation, :with => password_confirmation
+    click_button "Sign up"
+  end
+
 end
+
+
+
+
